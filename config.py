508b98cff5df0ff47e561ec44a115b9510c3a2d5
ahmetcangir <ahmet@statixdesign.com>
2019-06-12 04:15:27 +0200

    Modify gitignore

--- config.py
+++ config.py
@@ -12,21 +12,8 @@ DEBUG = True
 HOST = "0.0.0.0"
 
 # Secret key
-SECRET_KEY = "base64:VWRhY2l0eSBGdWxsIFN0YWNrIFdlYiBEZXZlbG9wZXIgTmFub2RlZ3JlZQ=="
+SECRET_KEY = "My Super Secret Key"
 
 # Database
 SQLALCHEMY_DATABASE_URI = "sqlite:///data/itemcatalog.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# Oauth credentials
-OAUTH_CREDENTIALS = {
-    "facebook": {"id": "355967214785654", "secret": "b82dd562d20668c95dc822ce6a1b55d6"},
-    "google": {
-        "id": "779916243668-f149orlcgna9crvhmji4m6sa4a5086eo.apps.googleusercontent.com",
-        "secret": "vMARc1lYx21dei8GEwoQLRxr",
-    },
-    "twitter": {
-        "id": "	EAhO6zLLTFaPsxElQEQxrHvaT",
-        "secret": "lUooJ0mMEPL3oo9k8cndvt9WR57tDimGxp8WQkC7ryO6BVvXRd",
-    },
-}
